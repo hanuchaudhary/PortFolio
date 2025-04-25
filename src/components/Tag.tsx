9107d22bd65e65b8e8126bbda759296341070686
hanuchaudhary <kushchaudharyog@gmail.com>
2025-04-25 18:34:35 +0530

    V2

--- src/components/Tag.tsx
+++
@@ -1,7 +0,0 @@
-export default function Tag({ tag }: { tag: string }) {
-  return (
-    <span className="rounded-full select-none bg-black dark:bg-neutral-100 dark:text-black text-white px-3 py-1 text-xs font-semibold border border-neutral-400/50">
-      {tag.trim()}
-    </span>
-  );
-}
