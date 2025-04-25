5650935eddbe1f9adc9076cf5a2e30a40547edbb
hanuchaudhary <kushchaudharyog@gmail.com>
2025-04-25 19:50:11 +0530

    V2Init

--- main/src/app/layout.tsx
+++ main/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SmoothScrolling from "./components/SmoothScrolling";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SmoothScrolling>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </SmoothScrolling>
       </body>
     </html>
   );
